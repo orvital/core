4b6ab12519eb57470c509a692f098160f5ab5f4e
Jona Goldman <jonagoldman@gmail.com>
2023-11-08 18:42:23 -0300

    Override core container aliases

--- src/Session/SessionServiceProvider.php
+++ src/Session/SessionServiceProvider.php
@@ -2,11 +2,26 @@
 
 namespace Orvital\Core\Session;
 
+use Illuminate\Contracts\Cache\Factory as CacheFactory;
+use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Session\SessionServiceProvider as BaseSessionServiceProvider;
 use Orvital\Core\Session\SessionManager;
 
 class SessionServiceProvider extends BaseSessionServiceProvider
 {
+    public function register()
+    {
+        $this->registerSessionManager();
+
+        $this->registerSessionDriver();
+
+        $this->app->singleton(StartSession::class, function ($app) {
+            return new StartSession($app->make(SessionManager::class), function () use ($app) {
+                return $app->make(CacheFactory::class);
+            });
+        });
+    }
+
     protected function registerSessionManager()
     {
         $this->app->singleton('session', function ($app) {
