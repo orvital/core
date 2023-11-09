b99d59e132f7f97fb938b361198e2763d54a63d3
Jona Goldman <jonagoldman@gmail.com>
2023-11-08 21:21:41 -0300

    Add auth traits and notifications

---
+++ src/Auth/Concerns/CanResetPassword.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Orvital\Core\Auth\Concerns;
+
+use Orvital\Core\Auth\Notifications\ResetPassword;
+
+/**
+ * @mixin \Orvital\Core\Database\Eloquent\Model
+ *
+ * @see \Illuminate\Contracts\Auth\CanResetPassword
+ */
+trait CanResetPassword
+{
+    /**
+     * Get the e-mail address where password reset links are sent.
+     *
+     * @return string
+     */
+    public function getEmailForPasswordReset()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPassword($token));
+    }
+}
