a0e2286e2dfe423ad0366398244f44efd14533b8
Jona Goldman <jonagoldman@gmail.com>
2023-12-16 19:14:06 -0300

    Remove Factory callbacks

--- src/Database/DatabaseServiceProvider.php
+++ src/Database/DatabaseServiceProvider.php
@@ -4,12 +4,10 @@ namespace Orvital\Core\Database;
 
 use Carbon\CarbonImmutable;
 use Illuminate\Database\DatabaseServiceProvider as BaseDatabaseServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Schema\Builder;
 use Illuminate\Support\Facades\Date;
-use Illuminate\Support\Str;
 
 /**
  * @property-read \Orvital\Core\Foundation\Application $app
@@ -31,39 +29,5 @@ class DatabaseServiceProvider extends BaseDatabaseServiceProvider
         Relation::requireMorphMap();
 
         Date::use(CarbonImmutable::class);
-
-        $this->bootFactoryCallbacks();
-    }
-
-    protected function bootFactoryCallbacks()
-    {
-        Factory::guessFactoryNamesUsing(function (string $modelName) {
-            $modelBase = class_basename($modelName);
-            $factoryBase = Str::finish($modelBase, 'Factory');
-
-            foreach ([
-                Str::replaceLast($modelBase, $factoryBase, $modelName),
-                Str::replaceLast($modelBase, 'Factories\\'.$factoryBase, $modelName),
-            ] as $factoryName) {
-                if (class_exists($factoryName)) {
-                    return $factoryName;
-                }
-            }
-
-            return Str::start($factoryBase, Factory::$namespace);
-        });
-
-        Factory::guessModelNamesUsing(function (Factory $factory) {
-            $modelName = Str::of(get_class($factory))
-                ->between(Factory::$namespace, 'Factory')
-                ->start($this->app->getNamespace())
-                ->toString();
-
-            if (class_exists($modelName)) {
-                return $modelName;
-            }
-
-            return $this->app->getNamespace().'Models\\'.class_basename($modelName);
-        });
     }
 }
