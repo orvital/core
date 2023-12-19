2f7b6d97e839a5b104f6d424b5f7daabfb310cd6
Jona Goldman <jonagoldman@gmail.com>
2023-12-18 23:57:26 -0300

    Refactor core aliases

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -8,54 +8,33 @@ use Illuminate\Foundation\Application as BaseApplication;
 
 class Application extends BaseApplication
 {
-    public function registerCoreContainerAliases()
+    public function __construct($basePath = null)
     {
-        foreach ([
-            'app' => [self::class, \Illuminate\Contracts\Container\Container::class, \Illuminate\Contracts\Foundation\Application::class, \Psr\Container\ContainerInterface::class],
-            'auth' => [\Illuminate\Auth\AuthManager::class, \Illuminate\Contracts\Auth\Factory::class],
-            'auth.driver' => [\Illuminate\Contracts\Auth\Guard::class],
-            'blade.compiler' => [\Illuminate\View\Compilers\BladeCompiler::class],
-            'cache' => [\Illuminate\Cache\CacheManager::class, \Illuminate\Contracts\Cache\Factory::class],
-            'cache.store' => [\Illuminate\Cache\Repository::class, \Illuminate\Contracts\Cache\Repository::class, \Psr\SimpleCache\CacheInterface::class],
-            'cache.psr6' => [\Symfony\Component\Cache\Adapter\Psr16Adapter::class, \Symfony\Component\Cache\Adapter\AdapterInterface::class, \Psr\Cache\CacheItemPoolInterface::class],
-            'config' => [\Illuminate\Config\Repository::class, \Illuminate\Contracts\Config\Repository::class],
-            'cookie' => [\Illuminate\Cookie\CookieJar::class, \Illuminate\Contracts\Cookie\Factory::class, \Illuminate\Contracts\Cookie\QueueingFactory::class],
-            'db' => [\Illuminate\Database\DatabaseManager::class, \Illuminate\Database\ConnectionResolverInterface::class],
-            'db.connection' => [\Illuminate\Database\Connection::class, \Illuminate\Database\ConnectionInterface::class],
-            'db.schema' => [\Illuminate\Database\Schema\Builder::class],
-            'encrypter' => [\Illuminate\Encryption\Encrypter::class, \Illuminate\Contracts\Encryption\Encrypter::class, \Illuminate\Contracts\Encryption\StringEncrypter::class],
-            'events' => [\Illuminate\Events\Dispatcher::class, \Illuminate\Contracts\Events\Dispatcher::class],
-            'files' => [\Illuminate\Filesystem\Filesystem::class],
-            'filesystem' => [\Illuminate\Filesystem\FilesystemManager::class, \Illuminate\Contracts\Filesystem\Factory::class],
-            'filesystem.disk' => [\Illuminate\Contracts\Filesystem\Filesystem::class],
-            'filesystem.cloud' => [\Illuminate\Contracts\Filesystem\Cloud::class],
-            'notification' => [\Illuminate\Notifications\ChannelManager::class, \Illuminate\Contracts\Notifications\Dispatcher::class, \Illuminate\Contracts\Notifications\Factory::class],
-            'hash' => [\Illuminate\Hashing\HashManager::class],
-            'hash.driver' => [\Illuminate\Contracts\Hashing\Hasher::class],
-            'translator' => [\Illuminate\Translation\Translator::class, \Illuminate\Contracts\Translation\Translator::class],
-            'log' => [\Illuminate\Log\LogManager::class, \Psr\Log\LoggerInterface::class],
-            'mail.manager' => [\Illuminate\Mail\MailManager::class, \Illuminate\Contracts\Mail\Factory::class],
-            'mailer' => [\Illuminate\Mail\Mailer::class, \Illuminate\Contracts\Mail\Mailer::class, \Illuminate\Contracts\Mail\MailQueue::class],
-            'auth.password' => [\Orvital\Core\Auth\Passwords\PasswordBrokerManager::class, \Illuminate\Contracts\Auth\PasswordBrokerFactory::class],
-            'auth.password.broker' => [\Orvital\Core\Auth\Passwords\PasswordBroker::class, \Illuminate\Contracts\Auth\PasswordBroker::class],
-            'queue' => [\Illuminate\Queue\QueueManager::class, \Illuminate\Contracts\Queue\Factory::class, \Illuminate\Contracts\Queue\Monitor::class],
-            'queue.connection' => [\Illuminate\Contracts\Queue\Queue::class],
-            'queue.failer' => [\Illuminate\Queue\Failed\FailedJobProviderInterface::class],
-            'redirect' => [\Illuminate\Routing\Redirector::class],
-            'redis' => [\Illuminate\Redis\RedisManager::class, \Illuminate\Contracts\Redis\Factory::class],
-            'redis.connection' => [\Illuminate\Redis\Connections\Connection::class, \Illuminate\Contracts\Redis\Connection::class],
-            'request' => [\Illuminate\Http\Request::class, \Symfony\Component\HttpFoundation\Request::class],
-            'router' => [\Illuminate\Routing\Router::class, \Illuminate\Contracts\Routing\Registrar::class, \Illuminate\Contracts\Routing\BindingRegistrar::class],
-            'session' => [\Orvital\Core\Session\SessionManager::class],
-            'session.store' => [\Orvital\Core\Session\Store::class, \Illuminate\Contracts\Session\Session::class],
-            'url' => [\Illuminate\Routing\UrlGenerator::class, \Illuminate\Contracts\Routing\UrlGenerator::class],
-            'validator' => [\Illuminate\Validation\Factory::class, \Illuminate\Contracts\Validation\Factory::class],
-            'view' => [\Illuminate\View\Factory::class, \Illuminate\Contracts\View\Factory::class],
-        ] as $key => $aliases) {
-            foreach ($aliases as $alias) {
-                $this->alias($key, $alias);
+        parent::__construct($basePath);
+
+        $this->overrideCoreContainerAliases();
+    }
+
+    public function overrideCoreContainerAliases()
+    {
+        $replacements = [
+            \Illuminate\Auth\Passwords\PasswordBrokerManager::class => \Orvital\Core\Auth\Passwords\PasswordBrokerManager::class,
+            \Illuminate\Auth\Passwords\PasswordBroker::class => \Orvital\Core\Auth\Passwords\PasswordBroker::class,
+            \Illuminate\Session\SessionManager::class => \Orvital\Core\Session\SessionManager::class,
+            \Illuminate\Session\Store::class => \Orvital\Core\Session\Store::class,
+        ];
+
+        $aliases = array_keys($this->aliases);
+
+        foreach ($replacements as $key => $value) {
+            if ($alias = $this->aliases[$key] ?? false) {
+                $this->abstractAliases[$alias][array_search($key, $this->abstractAliases[$alias])] = $value;
+
+                $aliases[array_search($key, $aliases)] = $value;
             }
         }
+
+        $this->aliases = array_combine($aliases, $this->aliases);
     }
 
     /**
@@ -66,6 +45,16 @@ class Application extends BaseApplication
     public function getInstances()
     {
         return $this->instances;
+    }
+
+    /**
+     * Get the container's aliases.
+     *
+     * @return array
+     */
+    public function getAliases()
+    {
+        return $this->aliases;
     }
 
     /**
@@ -111,7 +100,7 @@ class Application extends BaseApplication
      *
      * @throws BindingResolutionException
      */
-    public function makeAs(string $abstract, array $parameters = [], string $expected = null)
+    public function makeAs(string $abstract, array $parameters = [], ?string $expected = null)
     {
         $expected ??= $abstract;
 
